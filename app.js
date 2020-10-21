799ee3836ce25deb0a5bad548e072699492868da
czw <czwzy2017@gmail.com>
2020-10-21 17:41:19 +0800

    upload

---
+++ app.js
@@ -0,0 +1,35 @@
+import todoStore from './store/todoStore'
+
+//app.js
+App({
+  onLaunch: function () {
+    // 读取数据
+    todoStore.read()
+
+    // 获取用户信息
+    wx.getUserInfo({
+      success: (res) => {
+        // 可以将 res 发送给后台解码出 unionId
+        this.globalData.userInfo = res.userInfo
+      }
+    })
+  },
+
+  globalData: {
+    userInfo: null
+  },
+
+  /**
+   * 小程序隐藏事件
+   */
+  onHide() {
+    todoStore.save()
+  },
+
+  /**
+   * 小程序错误事件
+   */
+  onError() {
+    todoStore.save()
+  }
+})
---
+++ models/Todo.js
@@ -0,0 +1,31 @@
+import util from '../utils/util'
+
+/**
+ * Todo 模型类
+ */
+class Todo {
+  constructor (model) {
+    Object.assign(this, {
+      title: '',
+      desc: '',
+      date: new Date(),
+      level: 2,
+      completed: false,
+      createdAt: new Date(),
+      completedAt: new Date()
+    }, model)
+
+    // 日期格式化
+    if (this.date.constructor === Date) {
+      this.date = util.formatTime(this.date)
+    }
+    if (this.createdAt.constructor === Date) {
+      this.createdAt = util.formatTime(this.createdAt)
+    }
+    if (this.completedAt.constructor === Date) {
+      this.completedAt = util.formatTime(this.completedAt)
+    }
+  }
+}
+
+export default Todo
